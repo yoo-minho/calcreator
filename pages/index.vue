e49cfc5c553f841a0e8683a947674879e75124f5
유민호 <wingedcompany@gmail.com>
2024-07-08 23:34:53 +0900

    123

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 const page = useCookie("calcreator-page");
-if (page.value && page.value !== "/") {
+if (page.value && page.value !== "/" && page.value !== "/main") {
   await navigateTo(page.value, { replace: true, redirectCode: 301 });
 } else {
   await navigateTo("/main", { replace: true, redirectCode: 302 });
