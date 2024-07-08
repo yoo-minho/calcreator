26758ce3f1292a7b5fe48c1754b48928ae98f4d5
유민호 <wingedcompany@gmail.com>
2024-07-08 23:44:48 +0900

    123

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,9 @@ await delay(5000);
 if (page.value && page.value !== "/" && page.value !== "/main") {
   await navigateTo(page.value, { replace: true, redirectCode: 301 });
 } else {
-  await navigateTo("/main", { replace: true, redirectCode: 302 });
+  // await navigateTo("/main", { replace: true, redirectCode: 302 });
 }
 </script>
+<template>
+  <div>page:{{ page }}</div>
+</template>
