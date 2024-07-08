e2c01c736a1faf085a6617ddce4fdb2372972ffc
유민호 <wingedcompany@gmail.com>
2024-07-08 23:37:48 +0900

    123

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
+import { delay } from "~/util/CommUtil";
+
 const page = useCookie("calcreator-page");
+await delay(5000);
 if (page.value && page.value !== "/" && page.value !== "/main") {
   await navigateTo(page.value, { replace: true, redirectCode: 301 });
 } else {
   await navigateTo("/main", { replace: true, redirectCode: 302 });
 }
 </script>
-<template>
-  <div></div>
-</template>
-
-<style lang="scss" scoped></style>
